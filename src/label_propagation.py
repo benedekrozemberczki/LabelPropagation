025f0af7e22bc6a20f30f57977b3134e939467da
Do You Even Cross-Validate? <benedek.rozemberczki@gmail.com>
2018-12-30 12:09:09 +0000

    Update label_propagation.py

--- src/label_propagation.py
+++ src/label_propagation.py
@@ -3,7 +3,10 @@ from model import LabelPropagator
 from print_and_read import graph_reader, argument_printer
 
 def create_and_run_model(args):
-    
+    """
+    Method to run the model.
+    :param args: Arguments object.
+    """
     graph = graph_reader(args.input)
     model = LabelPropagator(graph, args)
     model.do_a_series_of_propagations()
@@ -12,6 +15,3 @@ if __name__ == "__main__":
     args = parameter_parser()
     argument_printer(args)
     create_and_run_model(args)
-
-
-
